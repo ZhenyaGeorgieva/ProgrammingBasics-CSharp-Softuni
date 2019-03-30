d4c75b14040b102b2e98863ffd9131ea4427afca
ZhenyaGeorgieva <zhenya.georgieva@gmail.com>
2019-03-30 19:48:20 +0200

    Changing Demo Exam files

---
+++ 07.DemoExam/07.DemoExam/04. Moving/Program.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _04.Moving
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int width = int.Parse(Console.ReadLine());
+            int length = int.Parse(Console.ReadLine());
+            int height = int.Parse(Console.ReadLine());
+
+
+            int volume = width * length * height;
+            while (true)
+            {
+                string command = Console.ReadLine();
+                if (command=="Done")
+                {
+                    break;
+                }
+                else
+                {
+                    int boxes = int.Parse(command);
+                    volume -= boxes;
+                    if(volume<=0)
+                    {
+                        break;
+                    }
+                }
+            }
+            if(volume>=0)
+            {
+                Console.WriteLine($"{volume} Cubic meters left.");
+            }
+            else
+            {
+                Console.WriteLine($"No more free space! You need {Math.Abs(volume)} Cubic meters more.");
+            }
+        }
+    }
+}
+
---
+++ 07.DemoExam/07.DemoExam/06.MultiplyTable/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _07Multiply_Table
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int threeDigitNumber = int.Parse(Console.ReadLine());
+            int thirdDigit = threeDigitNumber % 10;
+            int twoDigitNumber = threeDigitNumber / 10;
+            int secondDigit = twoDigitNumber % 10;
+            int firstDigit = twoDigitNumber / 10;
+
+            for (int i = 1; i <= thirdDigit; i++)
+            {
+                for (int j = 1; j <=secondDigit; j++)
+                {
+                    for (int z = 1; z <= firstDigit; z++)
+                    {
+                        if(i>0&&i!=0&&j>0&&j!=0&&z>0&&z!=0)
+                        {
+                            Console.WriteLine($"{i} * {j} * {z} = {i * j * z};");
+
+                        }
+                    }
+                }
+            }
+        }
+    }
+}

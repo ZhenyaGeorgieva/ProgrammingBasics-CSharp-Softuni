63a92787760939b2a4be2eb7f51b0abe5cbbf18b
ZhenyaGeorgieva <zhenya.georgieva@gmail.com>
2019-03-30 19:36:24 +0200

    Adding Demo Exam

---
+++ 07.DemoExam/01. SeaTrip/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _01.Sea_Trip
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double foodPerDay = double.Parse(Console.ReadLine());
+            double souvenirsPerDay = double.Parse(Console.ReadLine());
+            double hotelPerDay = double.Parse(Console.ReadLine());
+
+            double fuelNeeded = (420.0 / 100) * 7.0;
+            double moneyFuel = fuelNeeded * 1.85;
+            double moneySouvenirsFood = 3 * foodPerDay + 3 * souvenirsPerDay;
+            double firstHoteyDay = hotelPerDay * 0.9;
+            double secondHotelDay = hotelPerDay * 0.85;
+            double thirdHotelDay = hotelPerDay * 0.8;
+
+            double totalMoneyNeeded = moneyFuel + moneySouvenirsFood + firstHoteyDay + secondHotelDay + thirdHotelDay;
+            Console.WriteLine($"Money needed: {totalMoneyNeeded:f2}");
+        }
+    }
+}
+
---
+++ 07.DemoExam/03. SushiTime/Program.cs
@@ -0,0 +1,120 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _03.Sushi_Time
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string type = Console.ReadLine();
+            string restaurant= Console.ReadLine();
+            int portions = int.Parse(Console.ReadLine());
+            char delivery = char.Parse(Console.ReadLine());
+            double price = 0;
+            if (restaurant== "Sushi Zone")
+            {
+                if(type== "sashimi")
+                {
+                    price = 4.99;
+                }
+                    else if(type== "maki")
+                {
+                    price = 5.29;
+                }
+                    else if(type== "uramaki")
+                {
+                    price = 5.99;
+                }
+                    else if(type== "temaki")
+                {
+                    price = 4.29;
+                }
+                double total = portions * price;
+                if (delivery == 'Y')
+                { total = 1.2 * total; }
+                Console.WriteLine($"Total price: {Math.Ceiling(total)} lv.");
+            }
+            else if(restaurant == "Sushi Time")
+            {
+                if (type == "sashimi")
+                {
+                    price = 5.49;
+                }
+                else if (type == "maki")
+                {
+                    price = 4.69;
+                }
+                else if (type == "uramaki")
+                {
+                    price = 4.49;
+                }
+                else if (type == "temaki")
+                {
+                    price = 5.19;
+                }
+                double total = portions * price;
+                if (delivery == 'Y')
+                { total = 1.2 * total; }
+                Console.WriteLine($"Total price: {Math.Ceiling(total)} lv.");
+            }
+            else if(restaurant == "Sushi Bar")
+            {
+                if (type == "sashimi")
+                {
+                    price = 5.25;
+                }
+                else if (type == "maki")
+                {
+                    price = 5.55;
+                }
+                else if (type == "uramaki")
+                {
+                    price = 6.25;
+                }
+                else if (type == "temaki")
+                {
+                    price = 4.75;
+                }
+                double total = portions * price;
+                if (delivery == 'Y')
+                { total = 1.2 * total; }
+                Console.WriteLine($"Total price: {Math.Ceiling(total)} lv.");
+            }
+            else if(restaurant == "Asian Pub")
+            {
+                if (type == "sashimi")
+                {
+                    price = 4.50;
+                }
+                else if (type == "maki")
+                {
+                    price = 4.80;
+                }
+                else if (type == "uramaki")
+                {
+                    price = 5.50;
+                }
+                else if (type == "temaki")
+                {
+                    price = 5.50;
+                }
+                double total = portions * price;
+                if (delivery=='Y')
+                { total = 1.2 * total; }
+                Console.WriteLine($"Total price: {Math.Ceiling(total)} lv.");
+                
+            }
+                else
+            {
+                Console.WriteLine($"{restaurant} is invalid restaurant!");
+            }
+
+        }
+    }
+}
+
+
